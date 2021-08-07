129b0ab531d51ce5193f630767f3768325c361b5
Learned1 <84033125+Learned1@users.noreply.github.com>
2021-08-07 11:38:21 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,138 @@
+function StartGame () {
+    if (BGChoosen == 0) {
+        ZooPlayer = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . 5 . . . . . . 
+            . . . . . . 5 5 5 5 5 . . . . . 
+            . . . . . 5 f d d f 5 . . . . . 
+            . . . . . 5 d f f d 5 . . . . . 
+            . . . . . . . d d . . . . . . . 
+            . . . . 6 6 6 d d 6 6 6 . . . . 
+            . d d d 6 6 6 6 6 6 6 6 d d d . 
+            . d d d 6 6 6 6 6 6 6 6 d d d . 
+            . . . . 6 6 6 6 6 6 6 6 . . . . 
+            . . . . e e e e e e e e . . . . 
+            . . . . e e e e e e e e . . . . 
+            . . . . e e . . . . e e . . . . 
+            . . . . e e . . . . e e . . . . 
+            . . . . e e . . . . e e . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Player)
+    } else if (BGChoosen == 1) {
+        ZooPlayer = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . 5 . . . . . . 
+            . . . . . . 5 5 5 5 5 . . . . . 
+            . . . . . 5 f d d f 5 . . . . . 
+            . . . . . 5 d f f d 5 . . . . . 
+            . . . . . . . d d . . . . . . . 
+            . . . . 6 6 6 d d 6 6 6 . . . . 
+            . d d d 6 6 6 6 6 6 6 6 d d d . 
+            . d d d 6 6 6 6 6 6 6 6 d d d . 
+            . . . . 6 6 6 6 6 6 6 6 . . . . 
+            . . . . e e e e e e e e . . . . 
+            . . . . e e e e e e e e . . . . 
+            . . . . e e . . . . e e . . . . 
+            . . . . e e . . . . e e . . . . 
+            . . . . e e . . . . e e . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Player)
+    }
+}
+function start () {
+    textSprite = textsprite.create("")
+    BGcursor = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b b b b b . . . . 
+        . . . . . . b 1 1 1 1 b . . . . 
+        . . . . . b 1 1 1 1 1 b . . . . 
+        . . . . . . b 1 1 1 1 b . . . . 
+        . . . . . . b 1 1 1 1 b . . . . 
+        . . . . . b 1 b b 1 b . . . . . 
+        . . . . b 1 b . . b . . . . . . 
+        . . . . b b . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    Boy = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . 5 . . . . . . 
+        . . . . . . 5 5 5 5 5 . . . . . 
+        . . . . . 5 f d d f 5 . . . . . 
+        . . . . . 5 d f f d 5 . . . . . 
+        . . . . . . . d d . . . . . . . 
+        . . . . 6 6 6 d d 6 6 6 . . . . 
+        . d d d 6 6 6 6 6 6 6 6 d d d . 
+        . d d d 6 6 6 6 6 6 6 6 d d d . 
+        . . . . 6 6 6 6 6 6 6 6 . . . . 
+        . . . . e e e e e e e e . . . . 
+        . . . . e e e e e e e e . . . . 
+        . . . . e e . . . . e e . . . . 
+        . . . . e e . . . . e e . . . . 
+        . . . . e e . . . . e e . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    Girl = sprites.create(img`
+        . . . . . . b b e e . . . . . . 
+        . . . . . b e e e e e . . . . . 
+        . . . e e e 4 4 4 4 e e e . . . 
+        . . . . . 4 f b b f 4 . . . . . 
+        . . . . . 5 d f f d 5 . . . . . 
+        . . . . . 5 5 d d 5 5 . . . . . 
+        . . . . 3 3 3 d d 3 3 3 . . . . 
+        . d d d 3 3 3 3 3 3 3 3 d d d . 
+        . d d d 3 3 3 3 3 3 3 3 d d d . 
+        . . . . 3 3 3 3 3 3 3 3 . . . . 
+        . . . . e e e e e e e e . . . . 
+        . . . . e e e e e e e e . . . . 
+        . . . . e e . . . . e e . . . . 
+        . . . . e e . . . . e e . . . . 
+        . . . . e e . . . . e e . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    Boy.x = 20
+    Girl.x = 140
+    BGChoosing = 1
+}
+let BGChoosing = 0
+let Girl: Sprite = null
+let Boy: Sprite = null
+let BGcursor: Sprite = null
+let textSprite: TextSprite = null
+let ZooPlayer: Sprite = null
+let BGChoosen = 0
+start()
+game.onUpdate(function () {
+    if (BGChoosing == 1) {
+        if (controller.left.isPressed()) {
+            BGcursor.vx = -50
+        } else if (controller.right.isPressed()) {
+            BGcursor.vx = 50
+        } else {
+            BGcursor.vx = 0
+        }
+        if (BGcursor.overlapsWith(Boy)) {
+            textSprite.setText("Choose Boy?")
+            textSprite.setPosition(77, 33)
+            if (controller.A.isPressed()) {
+                BGChoosen = 0
+                StartGame()
+            }
+        } else if (BGcursor.overlapsWith(Girl)) {
+            textSprite.setText("Choose Girl?")
+            textSprite.setPosition(77, 33)
+            if (controller.A.isPressed()) {
+                BGChoosen = 1
+                StartGame()
+            }
+        } else {
+            textSprite.setText("")
+            textSprite.setPosition(73, 33)
+        }
+    }
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,90 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100003030303030303030303030303030303030303030303030404040404040403030303040405030505030304040304030303030403050503030503030303040303030206060605050304030303030403030303030506030505050303030304040303030505060303030505050303030404030305050606060606090505030303040303030306050503030403050303030403030504060303030303030305030303030504050605050505050303030403030305030506060606060705030303030303050505050505050505030303030303050303030503030303030303050303030303030303030303030303050303030303030303030303030303050503030303`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,sprites.castle.tileGrass1,sprites.castle.tileGrass2,sprites.castle.tileGrass3,sprites.castle.tilePath5,myTiles.tile3,myTiles.tile4,myTiles.tile5], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100002020202020202020202020202020202020202040202020202020202020202020105050505050502040202040204020202020202050705020202020202020202020202040505050602020303040202020202020205050202020202020203020202020206050202020202020303020202020202030505030204020203020202020202020403050602020402020202020202020202050502020202020202020202020208050505050505050802020202020202030202020202020202020204020202020202020202020203030302020202020202040202020202030203020202020204020202020303020202020202020202020202020202020202020202020202`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile3,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.castle.tileGrass2,sprites.castle.tilePath5,myTiles.tile6,myTiles.tile7,myTiles.tile8], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile5":
+            case "tile7":return tile7;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
